243a5e4b36db746046afb0706250c4a6b38293fc
samahmud49@gmail.com <samahmud49@gmail.com>
2024-01-03 20:29:46 +0600

    hamburger

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 let apiKey = 'https://ulvis.net/api.php?url=YOUR-LONG-URL&custom=YOUR-CUSTOM-NAME&private=1';
+
+
+
+let hamburger = document.querySelector('#hamburger');
+
+let sidebar = document.querySelector('#sidebar');
+
+document.onclick = e =>{
+  if(e.target.id !== 'sidebar' && e.target.id !== 'hamburger'){
+    hamburger.classList.remove('active');
+    sidebar.classList.remove('active');
+  }
+}
+
+
+hamburger.addEventListener('click', () =>{
+    hamburger.classList.toggle('active');
+    sidebar.classList.toggle('active');
+  });
+
+
